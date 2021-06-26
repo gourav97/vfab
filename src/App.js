335c0795af1e94ee2e56dedc6e4c085965b80802
Gourav <gouravapgis@gmail.com>
2021-06-26 14:47:18 +0530

    added items to collection

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import {Switch, Route} from 'react-router-dom';
 import Homepage from './pages/homepage/homepage.component';
-
+import ShopPage from './pages/shop/shop.component.jsx';
 
 const HatsPage = () => (
   <div>
@@ -15,7 +15,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path='/' component={Homepage} />
-        <Route  path='/hats' component={HatsPage} />
+        <Route  path='/shop' component={ShopPage} />
       </Switch>  
     </div>
   );
