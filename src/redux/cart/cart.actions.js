b99a07bd78158587b6d313ecc9651b45369ea074
Gourav <gouravapgis@gmail.com>
2021-06-28 20:00:53 +0530

    added cart utils

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -4,5 +4,12 @@ import { CartActionTypes } from "./cart.types"
 export const toggleCartHidden = () => ({
     type : CartActionTypes.TOOGLE_CART_HIDDEN
 
+
 });
 
+export const addItem = item => (
+    {
+        type : CartActionTypes.ADD_ITEM,
+        payload : item
+    }
+);
