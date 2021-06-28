07fd2362a8dc74c55e483f4b032fa6c5efd50b08
Gourav <gouravapgis@gmail.com>
2021-06-28 13:25:33 +0530

    added redux and implemented user reducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,6 +5,7 @@ import {auth} from '../../firebase/firebase.utils'
 import {ReactComponent as Logo} from '../../assests/crown.svg';
 
 import './header.styles.scss';
+import{ connect } from 'react-redux';
 
 const Header = ( {currentUser} ) => (
     <div className='header'>
@@ -25,4 +26,8 @@ const Header = ( {currentUser} ) => (
     </div>
 );
 
-export default Header;
+const mapStateToProps = state => ({
+    currentUser : state.user.currentUser
+})
+
+export default connect(mapStateToProps)(Header);
