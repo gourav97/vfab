b8209d2b5a619ea1f80043ec333cba2e34cc8b28
Gourav <gouravapgis@gmail.com>
2021-07-06 23:03:40 +0530

    moved shop data into its own reduer

--- src/index.js
+++ src/index.js
@@ -5,13 +5,18 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import reportWebVitals from './reportWebVitals';
-import store from './redux/store'
+import {store, persistor} from './redux/store'
+import { PersistGate } from 'redux-persist/integration/react';
+
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
+        
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,27 +1,21 @@
 import React from 'react';
-import SHOP_DATA from './shop.data.js';
-import CollectionPreview from '../../components/collecion-preview.component/collection-preview.component.jsx';
+import { Route} from 'react-router-dom';
 
-class ShopPage extends React.Component {
-    constructor(props) {
-        super(props);
-        
-        this.state = {
-            collections : SHOP_DATA              
-        }
-    }
 
-    render() {
-        const {collections} = this.state;
-        return (<div className='shop-page'>
-            {
-                collections.map(({id, ...otherCollectionProps}) => (
-                    <CollectionPreview key={id} {...otherCollectionProps}/>
-                ))
-            }
-            </div>);
+import CollectionOverview  from '../../components/collections-overview/collections-overview.components.jsx';
+import CollectionPage from '../collection/collection.component.jsx';
+
+const ShopPage = ({match}) => {
+    return(
+
+    <div className='shop-page'>
+       <Route exact path={`${match.path}`} component={CollectionOverview}/>
+       <Route path={`${match.path}/:collectionId`} component={CollectionPage}/>
         
-    }
-}
+    
+    </div>
+)};
+
+
 
 export default ShopPage;
